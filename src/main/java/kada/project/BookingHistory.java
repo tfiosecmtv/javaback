37f4780876b0b307f9fbd44031d6d9a517d9128e
Aidana Iman <aidana.imangozhina@nu.edu.kz>
2020-10-17 19:56:00 +0500

    added booking history crud

--- src/main/java/kada/project/BookingHistory.java
+++ src/main/java/kada/project/BookingHistory.java
@@ -1,4 +1,94 @@
 package kada.project;
+import javax.persistence.*;
+//import java.time.LocalDate;
 
+@Entity
+@Table(name = "bookinghistory")
 public class BookingHistory {
+    private Long booking_id;
+    private String room_type;
+    private String date_reservation;
+    private String due_date;
+    private String number_of_rooms;
+    private String payment_status;
+    private String appointment_status;
+
+
+
+    public BookingHistory(String room_type, String date_reservation, String due_date, String number_of_rooms, String payment_status, String appointment_status, Long booking_id) {
+        this.room_type = room_type;
+        this.date_reservation = date_reservation;
+        this.due_date = due_date;
+        this.number_of_rooms = number_of_rooms;
+        this.payment_status = payment_status;
+        this.appointment_status = appointment_status;
+        this.booking_id = booking_id;
+    }
+
+    public BookingHistory() {
+
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getBooking_id() {
+        return booking_id;
+    }
+    public void setBooking_id(Long booking_id) {
+        this.booking_id = booking_id;
+    }
+
+    @Column(name = "room_type")
+    public String getRoom_type() {
+        return room_type;
+    }
+    public void setRoom_type(String room_type) {
+        this.room_type = room_type;
+    }
+
+    @Column(name = "date_reservation")
+    public String getDate_reservation() {
+        return date_reservation;
+    }
+    public void setDate_reservation(String date_reservation) {
+        this.date_reservation = date_reservation;
+    }
+
+    @Column(name = "due_date")
+    public String getDue_date() {
+        return due_date;
+    }
+    public void setDue_date(String due_date) {
+        this.due_date = due_date;
+    }
+
+    @Column(name = "number_of_rooms")
+    public String getNumber_of_rooms() {
+        return number_of_rooms;
+    }
+    public void setNumber_of_rooms(String number_of_rooms) {
+        this.number_of_rooms = number_of_rooms;
+    }
+
+    @Column(name = "payment_status")
+    public String getPayment_status() {
+        return payment_status;
+    }
+    public void setPayment_status(String payment_status) {
+        this.payment_status = payment_status;
+    }
+
+    @Column(name = "appointment_status")
+    public String getAppointment_status() {
+        return appointment_status;
+    }
+    public void setAppointment_status(String appointment_status) {
+        this.appointment_status = appointment_status;
+    }
+
+
+    @Override
+    public String toString() {
+        return "BookingHistory [room_type=" + room_type + ", date_reservation=" + date_reservation + ", due_date=" + due_date + ", number_of_rooms=" + number_of_rooms + ", payment_status=" + payment_status + ", appointment_status=" + appointment_status + ", booking_id=" + booking_id + "]";
+    }
 }
---
+++ src/main/java/kada/project/BookingHistoryREST.java
@@ -0,0 +1,67 @@
+package kada.project;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/api")
+public class BookingHistoryREST {
+
+    @Autowired
+    private BookingHistoryRepo bookingHistoryRepo;
+
+    //get guests
+    @GetMapping("/bookinghistory")
+    public List<BookingHistory> getAllBookingHistory() {
+        return this.bookingHistoryRepo.findAll();
+    }
+
+    //get guests by email
+    @GetMapping("/bookinghistory/{booking_id}")
+    public ResponseEntity<BookingHistory> getBookingHistoryByBookingId(@PathVariable(value = "booking_id") Long booking_id) {
+        BookingHistory bookingHistory = bookingHistoryRepo.findById( booking_id ).orElseThrow();
+        return ResponseEntity.ok().body(bookingHistory);
+    }
+
+    //signup
+    @PostMapping("/bookinghistory")
+    public BookingHistory signup(@Validated @RequestBody BookingHistory bookingHistory) {
+        return bookingHistoryRepo.save(bookingHistory);
+    }
+    //update
+
+    @PutMapping("/bookinghistory/{booking_id}")
+    public ResponseEntity<BookingHistory> updatebooking(@PathVariable(value = "booking_id") Long booking_id,
+                                             @Validated @RequestBody BookingHistory bookingDets)  {
+        BookingHistory bookingHistory = bookingHistoryRepo.findById(booking_id)
+                .orElseThrow();
+
+        bookingHistory.setAppointment_status( bookingDets.getAppointment_status() );
+        bookingHistory.setBooking_id( bookingDets.getBooking_id() );
+        bookingHistory.setDate_reservation( bookingDets.getDate_reservation() );
+        bookingHistory.setDue_date( bookingDets.getDue_date() );
+        bookingHistory.setNumber_of_rooms( bookingDets.getNumber_of_rooms() );
+        bookingHistory.setPayment_status( bookingDets.getPayment_status() );
+        final BookingHistory updatedBooking = bookingHistoryRepo.save(bookingHistory);
+        return ResponseEntity.ok(updatedBooking);
+    }
+
+    //delete
+
+    @DeleteMapping("/bookinghistory/{booking_id}")
+    public Map<String, Boolean> deleteBooking(@PathVariable(value = "booking_id") Long booking_id)
+    {
+        BookingHistory bookingHistory = bookingHistoryRepo.findById(booking_id)
+                .orElseThrow();
+
+        bookingHistoryRepo.delete(bookingHistory);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
+
+}
